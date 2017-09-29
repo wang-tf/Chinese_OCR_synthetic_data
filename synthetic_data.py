2bbc8d4ce9f5287b40a83ea15f8ab90e780cb82d
ternencewang <ternencewang@126.com>
2017-09-29 20:30:28 +0800

    v1

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 import os
@@ -6,28 +6,28 @@ import random
 from PIL import Image, ImageDraw, ImageFont
 import utils 
 import codecs
+import tqdm
 
 
 def main():
-    data = OCRData()
-    args = data.setArguments()
-    print(args)
+    root_dir = './test_ocrdataset'
+    data = OCRData(root_dir)
+    data.makeNeededDir()
+    print('The arguments :')
+    print(data.args)
+    
     data.saveTopNCharacters2File(data.args['characters_file_path'], data.args['classes_number'], data.args['id_character_file_path'])
-    data.bg_img_list = utils.getBackgroundListFromDir(data.args['background_image_dir'])
-    print('Load backgroudn image: %d'%len(data.bg_img_list))
-    data.font_list = utils.getFontListFromDir(data.args['fonts_dir'])
     data.args['image_number'] = 20
     data.synthesizeAllImages(data.args['image_number'])
 
 
 class OCRData(object):
-    def __init__(self):
-        self.setArguments()
-        self.makeNeededDir()
+    def __init__(self, root_dir):
+        self.args = {}
+        self.args['root_dir'] = root_dir
+        args = self.setArguments()
 
     def setArguments(self):
-        self.args = {}
-        self.args['root_path'] = './test_dataset'
         self.args['characters_length_tuple'] = (3, 6)
         self.args['valiation_rate'] = 0.2
         self.args['test_rate'] = 0.2
@@ -38,35 +38,60 @@ class OCRData(object):
         self.args['id_character_file_path'] = os.path.basename(self.args['characters_file_path']).split('.')[0] + '_top_%d.txt'%self.args['classes_number']
         self.args['font_size_min'] = 32
         self.args['image_number'] = 10
+        self.args['save_full_image'] = 0
         return self.args
 
 
     def makeNeededDir(self):
-        utils.makeDirectory(self.args['root_path'])
-        self.train_image_dir = os.path.join(self.args['root_path'], 'train_img')
-        self.valiation_image_dir = os.path.join(self.args['root_path'], 'valiation_image')
-        self.test_image_dir = os.path.join(self.args['root_path'], 'test_image')
-        utils.makeDirectory(self.train_image_dir)
-        utils.makeDirectory(self.valiation_image_dir)
-        utils.makeDirectory(self.test_image_dir)
+        utils.makeDirectory(self.args['root_dir'])
+        self.makePartDirs('train')
+        if self.args['valiation_rate'] > 0:
+            self.makePartDirs('valiation')
+        if self.args['test_rate'] > 0:
+            self.makePartDirs('test')
+        self.args['annotations_dir'] = os.path.join(self.args['root_dir'], 'annotations')
+        utils.makeDirectory(self.args['annotations_dir'])
+        return
 
 
+    def makePartDirs(self, part_role):
+        if self.args['save_full_image']:
+            self.args[''.join([part_role,'_image_dir'])] = os.path.join(self.args['root_dir'], part_role+'_image')
+            utils.makeDirectory(self.args[part_role+'_image_dir'])
+        self.args[part_role+'_part_image_dir'] = os.path.join(self.args['root_dir'], part_role+'_part_image')
+        utils.makeDirectory(self.args[part_role+'_part_image_dir'])
+        return
+        
+        
     def synthesizeAllImages(self, image_number):
-        for i in range(image_number):
+        self.bg_img_list = utils.getBackgroundListFromDir(self.args['background_image_dir'])
+        self.font_list = utils.getFontListFromDir(self.args['fonts_dir'])
+        for i in tqdm.tqdm(range(image_number)):
             content, content_index = utils.get_contents(self.id_character_dict, self.args['characters_length_tuple'])
             background_image_path, font_path = map(utils.getRandomOneFromList, [self.bg_img_list, self.font_list])
             image, points = self.putContent2Image(content, background_image_path, font_path)
-            self.saveImage(image, i)
-            print(points)
+            if self.args['save_full_image']:
+                self.saveImage(image, i)
+            part_images, roi_points = utils.cropImageByPoints(image, points)
+            self.saveImage(part_images, i, is_part=1)
+            self.saveAnnotation(content_index, points, i)
         return
     
     
-    def saveImage(self, image, image_index):
-        image_save_dir = self.chooseSaveDirByIndex(image_index)
+    def saveImage(self, image, image_index, is_part=0):
+        image_save_dir = self.chooseSaveDirByIndex(image_index, is_part)
         utils.saveImage2Dir(image, image_save_dir, image_name=str(image_index))
 
-        
-    def putContent2Image(self, mulcontents, background_image_path, font_path):
+    
+    def saveAnnotation(self, content_index, points, image_index):
+        for index, one_content in enumerate(content_index):
+            ann_name = ''.join([str(image_index), '_', str(index), '.txt'])
+            ann_path = os.path.join(self.args['annotations_dir'], ann_name)
+            rectangle_points = utils.getOneLineRectanglePoints(points[index])
+            with codecs.open(ann_path, 'w', encoding='utf-8') as file:
+                file.write(' '.join([ann_name.split('.')[0], str(rectangle_points.tolist()), str(one_content)]))
+            
+    def putContent2Image(self, mulcontents, background_image_path, font_path, add_rectangle=0):
         try:
             image = Image.open(background_image_path)
             mulcontents_points = []
@@ -75,16 +100,19 @@ class OCRData(object):
                 image = image.resize((image.size[0]*2, image.size[1]*2))
                 font_size_max = image.size[0]/self.args['characters_length_tuple'][1]
             font_size = random.randint(self.args['font_size_min'], font_size_max)
-            left_top_point= (random.randint(0, image.size[0]-font_size*max([len(i) for i in mulcontents])), random.randint(0, image.size[1]-font_size*len(mulcontents)))
+            left_center_point= (random.randint(0, image.size[0]-font_size*max([len(i) for i in mulcontents])), random.randint(font_size*len(mulcontents), image.size[1]-font_size*len(mulcontents)/2))
+            color = utils.setColor(image)
             for content in mulcontents:
                 content_points = []
+                self.txt_center_line = 0
                 for character in content:
-                    image, points = self.putOneCharacter2Image(character, image, font_path, font_size, left_top_point)
+                    image, points = self.putOneCharacter2Image(character, image, font_path, font_size, left_center_point, color)
                     content_points.append(points)
-                    left_top_point = points[1]
-                left_top_point = content_points[0][3]
+                    left_center_point = (max(points[1][0], points[2][0]), left_center_point[1])
+                left_center_point = utils.getNewLeftCenterPointByContentPoints(content_points)
                 mulcontents_points.append(content_points)
-            image = utils.drawMulContentsRectangle(image, mulcontents_points)
+            if add_rectangle == 1:
+                image = utils.drawMulContentsRectangle(image, mulcontents_points)
             image_out = image
         except AssertionError:
             # print('MadeError, retry.')
@@ -92,30 +120,26 @@ class OCRData(object):
         return image_out, mulcontents_points
         
 
-    def putOneCharacter2Image(self, character, background_image, font_path, font_size, left_top_point):
+    def putOneCharacter2Image(self, character, background_image, font_path, font_size, left_center_point, color=None):
         background = background_image.convert('RGBA')
         font = ImageFont.truetype(font_path, font_size)
         width, height = font.getsize(character)
-        left, top = left_top_point
-        points = [left_top_point, (left+width, top), (left+width, top+height), (left, top+height)]  # clockwise
-        
         
         txt = Image.new('RGBA', (width, height), (255,255,255,0))
+        points_in_txt = utils.getPointsOfImageRectangle(width, height)
         draw = ImageDraw.Draw(txt)
-        # draw text, full opacity
-        draw.text((0, 0), character, font=font, fill=(255,255,255,255))
-        # draw.rectangle([points[0],points[2]])
-        # print(txt)
-        txt, points = utils.augmentImage(txt)
-        # print(txt)
-        pionts = utils.mergeBgimgAndTxtimgPoints(left_top_point, points)
+        draw.text((0, 0), character, font=font, fill=(255,255,255,255))  # draw text, full opacity
+        
+        txt, points_in_txt = utils.augmentImage(txt, points_in_txt)
+        points = utils.mergeBgimgAndTxtimgPoints(left_center_point, points_in_txt)
+        assert points[0][0] >= 0 and points[0][1] >= 0
         assert points[2][0] <= background.size[0] and points[2][1] <=background.size[1]
         # out_image = Image.alpha_composite(background, txt)
-        out_image = utils.mergeImageAtPoint(background, txt, left_top_point)
+        out_image = utils.mergeImageAtPoint(background, txt, tuple(points[0]), color)
         out_image = out_image.convert('RGB')        
         return out_image, points
-
-
+    
+    
     def saveTopNCharacters2File(self, characters_file_path, top_n, save_path):
         if 'id_character_dict' not in dict():
             self.id_character_dict = utils.getTopNCharacters2Dict(characters_file_path, top_n)
@@ -123,14 +147,23 @@ class OCRData(object):
         return
 
 
-    def chooseSaveDirByIndex(self, image_number_index):
+    def chooseSaveDirByIndex(self, image_number_index, is_part_img=0):
         train_rate = 1 - self.args['test_rate'] - self.args['valiation_rate']
         if image_number_index < int(train_rate * self.args['image_number']):
-            image_save_dir = self.train_image_dir
+            if not is_part_img:
+                image_save_dir = self.args['train_image_dir']
+            else:
+                image_save_dir = self.args['train_part_image_dir']
         elif image_number_index < int((train_rate + self.args['valiation_rate'])* self.args['image_number']):
-            image_save_dir = self.valiation_image_dir
+            if not is_part_img:
+                image_save_dir = self.args['valiation_image_dir']
+            else:
+                image_save_dir = self.args['valiation_part_image_dir']
         else:
-            image_save_dir = self.test_image_dir
+            if not is_part_img:
+                image_save_dir = self.args['test_image_dir']
+            else:
+                image_save_dir = self.args['test_part_image_dir']
         return image_save_dir
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 import os
@@ -6,10 +7,34 @@ import random
 import codecs
 import pathlib
 import math
+import PIL
 from PIL import Image, ImageDraw
 import numpy as np
 import cv2
 import Aug_Operations as aug
+
+
+def augmentImage(txt_img, points):
+    # Augment rate for eatch type
+    rot = random.uniform(0, 1)
+    skew_rate = random.uniform(0, 1)
+    shear_rate = random.uniform(0, 1)
+    distort_rate = random.uniform(0, 1)
+    
+    if rot < 1:
+        rot_degree = random.randint(-10, 10)
+        txt_img, points = rotate_img(txt_img, rot_degree)
+    elif skew_rate < 0:  # 平行四边形形变
+        skew = aug.Skew(1, 'RANDOM', 0.5)
+        txt_img, points = skew.perform_operation(txt_img)
+    elif shear_rate < 0:  # 剪切形变
+        shear= aug.Shear(1., 5, 5)
+        txt_img, points = shear.perform_operation(txt_img)
+    elif distort_rate < 0:  # 扭曲变形
+        distort = aug.Distort(1.0, 4, 4, 1)
+        txt_img = distort.perform_operation(txt_img)
+    return txt_img, points 
+
 
 def getTopNCharacters2Dict(characters_file_path, top_n):
     id_cha_dict = {}
@@ -23,7 +48,6 @@ def getAllCharactersFromFile(characters_file_path):
     characters_set = set()
     with codecs.open(characters_file_path, encoding="utf-8") as file:
         lines = file.readlines()
-        print('lines')
         for oneline in lines:
                 characters_set.update(list(oneline.replace(' ','')))
     return list(characters_set)
@@ -38,11 +62,13 @@ def makeDirectory(path):
 def getBackgroundListFromDir(background_dir):
     image_path_list = []
     image_path_list.extend([path for path in pathlib.Path(background_dir).rglob('*.jpg')])
+    print('Load backgroudn image: %d'%len(image_path_list))
     return image_path_list
 
 def getFontListFromDir(font_dir):
     font_path_list = []
     font_path_list.extend(glob.glob(os.path.join(font_dir, '*.[t,T][t,T][f,F,c,C]')))
+    print('Load font files: %d'%len(font_path_list))
     return font_path_list
 
 def get_content(id_character_dict, length_range_tuple):
@@ -54,7 +80,6 @@ def get_content(id_character_dict, length_range_tuple):
         rand_index = random.randint(0, length-1)
         content += id_character_dict[rand_index]
         content_index.append(rand_index)
-    print(content)
     return content, content_index
 
 def get_contents(id_character_dict, length_range_tuple, line_number=2):
@@ -66,48 +91,27 @@ def get_contents(id_character_dict, length_range_tuple, line_number=2):
     return contents, contents_index
 
 def saveImage2Dir(image, image_save_dir, image_name='test_image'):
-    image_save_path = os.path.join(image_save_dir, image_name+'.jpg')
-    image.save(image_save_path)
+    if type(image) == list:
+        for index, one_image in enumerate(image):
+            saveImage2Dir(one_image, image_save_dir, image_name=image_name+'_'+str(index))
+    else:
+        image_save_path = os.path.join(image_save_dir, image_name+'.jpg')
+        image.save(image_save_path)
 
-
-def augmentImage(txt_img):
-    # Augment rate for eatch type
-    rot = random.uniform(0, 1)
-    skew_rate = random.uniform(0, 1)
-    shear_rate = random.uniform(0, 1)
-    distort_rate = random.uniform(0, 1)
-    
-    if rot < 0:  # 旋转
-        rot_degree = random.randint(-10, 10)
-        txt_img, points = rotate_img(txt_img, rot_degree)
-    elif skew_rate < 1:  # 平行四边形形变
-        skew = aug.Skew(1, 'RANDOM', 0.5)
-        txt_img, points = skew.perform_operation(txt_img)
-    elif shear_rate < 1:  # 剪切形变
-        shear= aug.Shear(1., 5, 5)
-        txt_img, points = shear.perform_operation(txt_img)
-    elif distort_rate < 1:  # 扭曲变形
-        distort = aug.Distort(1.0, 4, 4, 1)
-        txt_img = distort.perform_operation(txt_img)
-    return txt_img, points 
-
-# 旋转图像并保留原图像的全部区域
 def rotate_img(image, degree):
-
     img = np.array(image)
 
     height, width = img.shape[:2]
 
-    # 旋转后的尺寸
-    heightNew = int(width * fabs(sin(radians(degree))) + height * fabs(cos(radians(degree))))
-    widthNew = int(height * fabs(sin(radians(degree))) + width * fabs(cos(radians(degree))))
+    heightNew = int(width * math.fabs(math.sin(math.radians(degree))) + height * math.fabs(math.cos(math.radians(degree))))
+    widthNew = int(height * math.fabs(math.sin(math.radians(degree))) + width * math.fabs(math.cos(math.radians(degree))))
 
     matRotation = cv2.getRotationMatrix2D((width / 2, height / 2), degree, 1)
 
-    matRotation[0, 2] += (widthNew - width) / 2  # 重点在这步，目前不懂为什么加这步
-    matRotation[1, 2] += (heightNew - height) / 2  # 重点在这步
-
-    imgRotation = cv2.warpAffine(img, matRotation, (widthNew, heightNew), borderValue=(0, 0, 0))
+    matRotation[0, 2] += (widthNew - width) / 2  
+    matRotation[1, 2] += (heightNew - height) / 2  
+    
+    imgRotation = cv2.warpAffine(img, matRotation, (widthNew, heightNew), borderValue=(255, 255, 255, 0))
 
     w = width
     h = height
@@ -121,15 +125,16 @@ def rotate_img(image, degree):
 
     p = matRotation * points.T
 
-    #cv2.imshow("img", img)
-    #cv2.imshow("imgRotation", imgRotation)
-    #cv2.waitKey(0)
-
+    for row in imgRotation:
+        for element in row:
+            if element[3] == 0:
+                for i in xrange(3):
+                    element[i] = 0
     image = Image.fromarray(imgRotation)
+    points = np.array(p.T, int)
+    return image, points
 
-    return image, p.T
-
-def mergeImageAtPoint(image, txt_img, left_top_point):
+def mergeImageAtPoint(image, txt_img, left_top_point, color):
     left, top = left_top_point
     image  = pltImage2Array(image)
     
@@ -138,7 +143,7 @@ def mergeImageAtPoint(image, txt_img, left_top_point):
     res_img = np.array(image)
 
     roi_img = image[top:(top+h), left:(left+w),:]
-    color = setColor(roi_img)
+    if len(color) != 3: color = setColor(roi_img)
 
     txt_img = np.array(txt_img)
     mask = txt_img[:, :, 0]
@@ -153,13 +158,16 @@ def mergeImageAtPoint(image, txt_img, left_top_point):
 
     return res_img
 
-def mergeBgimgAndTxtimgPoints(left_top_point, points):
-    left, top = left_top_point
+def mergeBgimgAndTxtimgPoints(left_center, points):
+    left, center_line = left_center
+    top = center_line - (max(points[:, 1]) - min(points[:, 1]))/2
     for k in range(0, 4):
         points[k, :] += np.array([left, top])
     return points
 
 def setColor(roi_img):
+    if type(roi_img) != np.ndarray:
+        roi_img = np.array(roi_img)
     color1 = cv2.mean(roi_img)
     color = np.zeros(3, dtype=np.int)
     color[0] = math.ceil(color1[2])
@@ -190,9 +198,65 @@ def drawMulContentsRectangle(image, mulcontents_points):
     draw = ImageDraw.Draw(image)
     for content_points in mulcontents_points:
         for point in content_points:
-            draw.rectangle((tuple(point[0]), tuple(point[2])))
+            draw.line([tuple(point[0]), tuple(point[1]), tuple(point[2]), tuple(point[3]), tuple(point[0])])
+            # draw.rectangle((tuple(point[0]), tuple(point[2])))
     del draw
     return image
 
 def getRandomOneFromList(list):
     return list[random.randint(0, len(list)-1)]
+
+def getPointByCenterLine(center_line, left_top_point, width, height):
+    left, _ = left_top_point
+    points = [(left, center_line - height/2), 
+           (left + width, center_line - height/2), 
+           (left + width, center_line + height/2), 
+           (left, center_line + height/2)]
+    return points
+
+def getNewLeftCenterPointByContentPoints(content_points):
+    left = content_points[0][0][0]
+    bottom_line = getTopOrBottomLineInPoints(content_points, is_top=0)
+    top_line = getTopOrBottomLineInPoints(content_points, is_top=1)
+    height = bottom_line - top_line
+    return (left, bottom_line+height/2)
+
+def getTopOrBottomLineInPoints(points, is_top):
+    list = []
+    if is_top:
+        for i in points:
+            list.append(min(i[:, 1]))
+        return min(list)
+    else:
+        for i in points:
+            list.append(max(i[:, 1]))
+        return max(list)
+    
+def getPointsOfImageRectangle(width, height):
+    return np.array([[0, 0], [width, 0], [width, height], [0, height]])
+
+def cropImageByPoints(image, points,):
+    part_image= []
+    roi_points_list = []
+    for oneline in points:
+        roi = getOneLineRectanglePoints(oneline)
+        random_roi_tuple = addRandomInROI(roi)
+        part_image.append(image.crop(random_roi_tuple))
+        roi_points_list.append(roi)
+    return part_image, roi_points_list
+
+def addRandomInROI(roi):
+    left, top, right, bottom = (roi[0][0],roi[0][1],roi[2][0],roi[2][1])
+    max_randint = (bottom - top) / 6
+    new_left = random.randint(left-max_randint, left+max_randint/2)
+    new_top = random.randint(top-max_randint, top+max_randint/2)
+    new_right = random.randint(right-max_randint/2, right+max_randint)
+    new_bottom = random.randint(bottom, bottom+2*max_randint)
+    return (new_left, new_top, new_right, new_bottom)
+    
+def getOneLineRectanglePoints(one_line_points):
+    top = getTopOrBottomLineInPoints(one_line_points, is_top=1)
+    bottom = getTopOrBottomLineInPoints(one_line_points, is_top=0)
+    left = min(one_line_points[0][0][0], one_line_points[0][3][0])
+    right = max(one_line_points[-1][1][0], one_line_points[-1][2][0])
+    return np.array([[left, top], [right, top], [right, bottom], [left, bottom]])
